73193cd30591c93b42c66f0fe54bb413a6ebdc70
hankun <349920515@qq.com>
2018-12-18 16:56:10 +0800

    Add files via upload

---
+++ getdir.py
@@ -0,0 +1,17 @@
+import os
+
+srcdir=r'原始库'
+distdir=r'bigimg'
+
+
+l1srcdirs=os.listdir(srcdir)
+
+for l1srcdir in l1srcdirs:
+    l1fullpath=os.path.join(srcdir,l1srcdir)
+    if(os.path.isdir(l1fullpath)):
+        os.makedirs(os.path.join(distdir,l1srcdir))
+        l2srcdirs=os.listdir(l1fullpath)
+        for l2srcdir in l2srcdirs:
+            l2fullpath=os.path.join(l1fullpath,l2srcdir)
+            if os.path.isdir(l2fullpath):
+                os.makedirs(os.path.join(distdir,l1srcdir,l2srcdir))
