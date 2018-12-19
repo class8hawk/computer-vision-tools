843d3099de38c59050bcc89c885fd22f7c0868f3
hawk <349920515@qq.com>
2018-12-19 14:16:17 +0800

    已有目录则不创建

--- getdir.py
+++ getdir.py
@@ -9,9 +9,11 @@ l1srcdirs=os.listdir(srcdir)
 for l1srcdir in l1srcdirs:
     l1fullpath=os.path.join(srcdir,l1srcdir)
     if(os.path.isdir(l1fullpath)):
-        os.makedirs(os.path.join(distdir,l1srcdir))
+        if not os.path.exists(os.path.join(distdir,l1srcdir)):
+            os.makedirs(os.path.join(distdir,l1srcdir))
         l2srcdirs=os.listdir(l1fullpath)
         for l2srcdir in l2srcdirs:
             l2fullpath=os.path.join(l1fullpath,l2srcdir)
             if os.path.isdir(l2fullpath):
-                os.makedirs(os.path.join(distdir,l1srcdir,l2srcdir))
+                if not os.path.exists(os.path.join(distdir,l1srcdir,l2srcdir)):
+                    os.makedirs(os.path.join(distdir,l1srcdir,l2srcdir))
