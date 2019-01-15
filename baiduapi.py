c15f4fb368bf128be15f87d3ac43a6fa9250e502
class8hawk <349920515@qq.com>
2019-01-15 21:58:27 +0800

    baiduapi

--- baiduapi.py
+++ baiduapi.py
@@ -2,7 +2,7 @@ import urllib, urllib.request, sys
 import ssl
 
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=v2aMfYDr7PvcI6DGUoh9byEh&client_secret=hxFIGWcr9IDs43j49giIk3HkLiTkqEPt'
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=xxxxx&client_secret=xxxxxx'
 request = urllib.request.Request(host)
 request.add_header('Content-Type', 'application/json; charset=UTF-8')
 response = urllib.request.urlopen(request)
