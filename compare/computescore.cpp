41751b5139ebc557a9751d0a0888347730a7b50e
hawk <349920515@qq.com>
2019-12-10 12:13:30 +0800

    a

--- compare/computescore.cpp
+++ compare/computescore.cpp
@@ -17,6 +17,42 @@
 #include <string>
 using namespace std;
 
+
+class filesnode
+{
+  private:
+   filesnode()
+   {
+
+   }
+   int m;
+  
+   int k;
+  public:
+   filesnode(int m,int k)
+   {
+      this->m=m;
+      this->k=k;
+       A = (float*)malloc(sizeof(float)*m*k);
+       size=0;
+   }
+   float *A;
+   vector<string> id;
+   int size;
+    ~filesnode()
+   {
+       
+   }
+
+
+   
+
+
+
+};
+
+
+
 int getAbsoluteFiles(string directory, list<string>& filesAbsolutePath) //参数1[in]要变量的目录  参数2[out]存储文件名
 {
     DIR* dir = opendir(directory.c_str()); //打开目录   DIR-->类似目录句柄的东西 
@@ -105,6 +141,49 @@ static void Split_string(const string& strSrc, const string& splitCh, vector<str
 
 }
 
+void readfeaturefromtxt(filesnode& node,list<string>& file,int k)
+{
+           int A_index=0;
+            node.size=file.size();
+           for(list<string>::iterator it = file.begin();it!=file.end();it++) //readtxt
+           {
+              
+              vector<string> splits;
+              const string dirname=*it;
+              //cout<<dirname<<endl;
+              Split_string(dirname,"/",splits);
+              //cout<<splits[splits.size()-2]<<endl;
+              node.id.push_back(splits[splits.size()-2]);
+              
+              ifstream readFile(*it);
+
+
+
+              //cout<<*it<<endl;
+               float temp;
+               for(int kindex=0;kindex<k;kindex++)
+               {
+                 readFile >>temp;   
+                 node.A[A_index*k+kindex]=temp;
+                 //cout<<node.A[A_index*k+kindex]<<endl;
+
+
+               }
+              readFile.close();
+              
+              
+              A_index++;
+
+           }
+
+
+
+
+
+
+}
+
+
 void readfeaturefromtxt(float* A,list<string>& file,int k,vector<string>& idstring)
 {
            int A_index=0;
@@ -153,14 +232,15 @@ void readfeaturefromtxt(float* A,list<string>& file,int k,vector<string>& idstri
 int main(void)
 {
    int batchsize=30000;
+   //int batchsize=100;
 
     int const m = batchsize;
     int const n = batchsize;
     int const k =512;
-    float *A ,*B,*C;
+    float *C;
     float *d_A,*d_B,*d_C;
-    A = (float*)malloc(sizeof(float)*m*k);  
-    B = (float*)malloc(sizeof(float)*n*k);  
+    //A = (float*)malloc(sizeof(float)*m*k);  
+    //B = (float*)malloc(sizeof(float)*n*k);  
     C = (float*)malloc(sizeof(float)*m*n); 
     
     float alpha = 1.0;
@@ -216,44 +296,63 @@ int main(void)
 
             }
         }
+        cout<<"readtxt to filenode"<<endl;
+        vector<filesnode> filenodes;
+        
         for(int i=0;i<leftdirproc.size();i++)
         {
-           cout<<i<<"   total:"<<leftdirproc.size()<<endl;
-           vector<string> Aidstring;
+             cout<<"read:"<<i<<"   total:"<<leftdirproc.size()<<endl;
+             filesnode temp(m,k);
+             readfeaturefromtxt(temp,leftdirproc[i],k);
+             filenodes.push_back(temp);
+
+        } 
+
+
+
+
+        for(int i=0;i<leftdirproc.size();i++)
+        {
+           cout<<"proc:"<<i<<"   total:"<<leftdirproc.size()<<endl;
+           //vector<string> Aidstring;
            
-           readfeaturefromtxt(A,leftdirproc[i],k,Aidstring);
+           //readfeaturefromtxt(A,leftdirproc[i],k,Aidstring);
            //return 0;
            for (int j=i;j<leftdirproc.size();j++)
            {
              cout<<"L2:"<<j<<"   total:"<<leftdirproc.size()<<endl;
-               vector<string> Bidstring;
-               readfeaturefromtxt(B,leftdirproc[j],k,Bidstring);
-                
-               cudaMemcpy(d_A,A,sizeof(float)*m*k,cudaMemcpyHostToDevice);
-               cudaMemcpy(d_B,B,sizeof(float)*n*k,cudaMemcpyHostToDevice);
+              // vector<string> Bidstring;
+               //readfeaturefromtxt(B,leftdirproc[j],k,Bidstring);
+                //cout<<filenodes[i].A<<"  "<<filenodes[j].A<<endl;
+               cudaMemcpy(d_A,filenodes[i].A,sizeof(float)*m*k,cudaMemcpyHostToDevice);
+               cudaMemcpy(d_B,filenodes[j].A,sizeof(float)*n*k,cudaMemcpyHostToDevice);
 
               
                cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, k, &alpha, d_A, k, d_B, k, &beta, d_C, m);//<测试二>
                //cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, m, n, k, &alpha, d_A, m, d_B, n, &beta, d_C, n);
                cudaMemcpy(C,d_C,sizeof(float)*m*n,cudaMemcpyDeviceToHost);
-               for (int i = 0; i< m*n;i++){
+               for (int i_index = 0; i_index< m*n;i_index++){
                 //std::cout <<C[i]<<"\t";
-                if(C[i]>0.5)
+                if(C[i_index]>0.55)
                 {
-                    int aindex=i%m;
-                    int bindex=i/m;
-                    if(aindex>=Aidstring.size()||bindex>=Bidstring.size())
+                    int aindex=i_index%m;
+                    int bindex=i_index/m;
+                   // cout<<i_index<<"  "<<i<<"  "<<j<<"  "<<endl;
+                    //cout<<
+                    //cout<<filenodes[i].id.size()<<endl;
+                    if(aindex>=filenodes[i].id.size()||bindex>=filenodes[j].id.size())
+                    {
+                       // cout<<"pass"<<endl;
+                        continue;
+                    }
+                    if(filenodes[i].id[aindex]==filenodes[j].id[bindex])
                     {
                         continue;
                     }
-                    if(Aidstring[aindex]==Bidstring[bindex])
-                    {
-                        continue;
-                    }
-                    string saveresname="res/"+Aidstring[aindex]+"_"+Bidstring[bindex]+"_"+to_string(int(C[i]*100))+".txt";
+                    string saveresname="res/"+filenodes[i].id[aindex]+"_"+filenodes[j].id[bindex]+"_"+to_string(int(C[i_index]*100))+".txt";
                     ofstream OutFile(saveresname); //利用构造函数创建txt文本，并且打开该文本
 
-                    OutFile << Aidstring[aindex]<<"    "<<Bidstring[bindex]<<"    "<<C[i]<<endl;;  //把字符串内容"This is a Test!"，写入Test.txt文件
+                    OutFile << filenodes[i].id[aindex]<<"    "<<filenodes[j].id[bindex]<<"    "<<C[i_index]<<endl;;  //把字符串内容"This is a Test!"，写入Test.txt文件
 
                     OutFile.close();         
 
@@ -264,6 +363,15 @@ int main(void)
            }
 
         }
+
+       for(int i=0;i<filenodes.size();i++)
+        {
+           free(filenodes[i].A);
+
+        }
+
+
+
     }
     
    
@@ -271,8 +379,8 @@ int main(void)
 
    
   
-    free(A);
-    free(B);
+   // free(A);
+   // free(B);
     free(C);
     cudaFree(d_A);
     cudaFree(d_B);
