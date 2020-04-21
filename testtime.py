a8d99e1944451d6c1f3e966676b527c99b3d2531
hankun_hawk <349920515@qq.com>
2020-04-21 16:05:05 +0800

    time

--- testtime.py
+++ testtime.py
@@ -2,7 +2,7 @@ import datetime
 
 
 time1=datetime.datetime.now()
-time1_str = datetime.datetime.strftime(time1,'%Y-%m-%d %H:%M:%S')
+time1_str = datetime.datetime.strftime(time1,'%Y-%m-%d-%H-%M')
 
 
 print(time1_str)
