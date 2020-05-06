618d5427135f68d67a47306b9e8f295ca0a7de74
hankun_hawk <349920515@qq.com>
2020-05-06 16:08:19 +0800

    a

--- fc1_plate.py
+++ fc1_plate.py
@@ -15,19 +15,19 @@ imgdir='71.bmp'   #随机找的一张待测图片
  
 def Test(img):
       
-    net = caffe.Net(deploy,caffe_model,caffe.TEST)   #加载model和network 
+    #net = caffe.Net(deploy,caffe_model,caffe.TEST)   #加载model和network 
        
     #图片预处理设置 
-    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})  #设定图片的shape格式(1,3,28,28) 
-    transformer.set_transpose('data', (2,0,1))    #改变维度的顺序，由原始图片(28,28,3)变为(3,28,28) 
+    #transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})  #设定图片的shape格式(1,3,28,28) 
+    #transformer.set_transpose('data', (2,0,1))    #改变维度的顺序，由原始图片(28,28,3)变为(3,28,28) 
     #transformer.set_mean('data', np.load(mean_file).mean(1).mean(1))    #减去均值，前面训练模型时没有减均值，这儿就不用
     #transformer.set_mean('data',  np.array([128,128,128]))
-    transformer.set_raw_scale('data', 255)    # 缩放到【0，255】之间 
+    #transformer.set_raw_scale('data', 255)    # 缩放到【0，255】之间 
     #transformer.set_channel_swap('data', (2,1,0))   #交换通道，将图片由RGB变为BGR 
     #transformer.set_input_scale('data',1/128)
     #transformer.set_input_scale('data',0.017)
-    im=caffe.io.load_image(imgdir)                   #加载图片 
-    net.blobs['data'].data[...] = transformer.preprocess('data',im)      #执行上面设置的图片预处理操作，并将图片载入到blob中 
+    #im=caffe.io.load_image(imgdir)                   #加载图片 
+    #net.blobs['data'].data[...] = transformer.preprocess('data',im)      #执行上面设置的图片预处理操作，并将图片载入到blob中 
        
     #执行测试 
     #net.params['bn_data'][0].data=np.array([1,1,1],dtype='float32')
@@ -37,6 +37,14 @@ def Test(img):
     print("bn_data_scale:",filters)
     #print("blobs {}\nparams {}".format(net.blobs.keys(), net.params.keys()))
     #net.save('pos_C5_RGB_fixgamma.caffemodel');
+    im=cv2.imread(imgdir)
+    inp_image = ((im / 255. - [0.408, 0.447, 0.470]) / [0.289, 0.274, 0.278]).astype(np.float32)
+    images = inp_image.transpose(2, 0, 1).reshape(1, 3, 254, 254)
+    print(images)
+    
+    net.blobs['blob1'].data[...] = images 
+    
+    
     out = net.forward() 
     datamiddle= net.blobs['data'].data[0]
     print(datamiddle)
